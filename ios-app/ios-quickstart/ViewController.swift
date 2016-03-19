481389087f07ff327d28ada6b6241508c0dab1c8
Andrew Wagner <wagner.andrew@gmail.com>
2016-03-19 14:48:06 -0400

    add in ios-quickstart app

---
+++ ios-app/ios-quickstart/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  ios-quickstart
+//
+//  Created by Andrew Wagner on 3/14/16.
+//  Copyright © 2016 Andrew Wagner. All rights reserved.
+//
+
+import UIKit
+import WatsonDeveloperCloud
+
+class ViewController: UIViewController {
+    var player: AVAudioPlayer?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let tts = TextToSpeech(username: "5fd5ef73-28a2-4b85-a7bd-b6d1c51c3a39", password: "sFYpVkqvTIc0")
+        tts.synthesize("All the problems of the world could be settled easily if men were only willing to think.") {
+            
+            data, error in
+            
+            if let audio = data {
+                
+                do {
+                    self.player = try AVAudioPlayer(data: audio)
+                    self.player!.play()
+                } catch {
+                    print("Couldn't create player.")
+                }
+                
+            } else {
+                print(error)
+            }
+            
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
